b5db09d6d7de7c2cd5dec0de49baef5ffc5adfa5
liuxiaoxuan97 <54862568+liuxiaoxuan97@users.noreply.github.com>
2020-12-11 10:17:38 -0500

    Update dice_coef.py

--- utils/dice_coef.py
+++ utils/dice_coef.py
@@ -3,7 +3,7 @@ def dice_coef(y_true, y_pred):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + 1) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1)
+    return (2.0 * intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1.0)
 
 #y_pred is predicted probability of classes 
 def dice_coef_numpy(y_true,y_pred):
@@ -11,4 +11,4 @@ def dice_coef_numpy(y_true,y_pred):
     y_pred_f= K.flatten((y_pred>0.5).astype('uint8')).numpy()
     intersection=sum(y_true_f*y_pred_f)
     k=(sum(y_true_f) + sum(y_pred_f))
-    return 2.*intersection+1.0/ (k+1)
+    return (2.0*intersection+1.0)/ (k+1)
